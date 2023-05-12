ebe29e33d97c7b9e515ca2beaec1a51246f6d25f
huyhuynhC0223G1 <huynhduchuy403@gmail.com>
2023-05-12 11:08:22 +0700

    bai tap ngay 12/05

---
+++ ss1_csdl/bai_tap/bai1.sql
@@ -0,0 +1,16 @@
+create database bai1;
+
+create table class(
+id int primary key,
+`name` varchar(45)
+);
+
+create table teacher(
+id int primary key,
+`name` varchar(45),
+age int,
+country varchar(45)
+);
+
+select * from class;
+select * from teacher;
---
+++ ss1_csdl/thuc_hanh/csdl.sql
@@ -0,0 +1,9 @@
+create database if not exists mydatabase;
+
+create table mytable(
+id int primary key,
+`name` varchar(45),
+age int
+);
+
+select * from mytable;
